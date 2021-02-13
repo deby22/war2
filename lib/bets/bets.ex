d594cbdcb48dd0113bcb97f75c028ba970a2d85c
Dawid Dęby <dawid.deby@gmail.com>
2021-02-13 14:23:37 +0100

    bets dialyzer

--- lib/bets/bets.ex
+++ lib/bets/bets.ex
@@ -1,7 +1,10 @@
 defmodule Bets.Bets do
   alias Bets.Bet
 
-  @spec create_bet(map) :: {:ok, Bets.Bet} || {:errors, list()}
+  @type invalid_bet_message :: {:errors, list(String.t())}
+  @type validated_bet :: {:ok, %Bets.Bet{}}
+
+  @spec create_bet(map()) :: validated_bet | invalid_bet_message
   def create_bet(bet = %{}) do
     validated_bet = Bet.validate_bet(struct(Bet, bet))
 
