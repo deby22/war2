edf2f63bf3d3deb21bf6a0b948de82bc3a9fd80a
Dawid Dęby <dawid.deby@gmail.com>
2021-02-13 14:12:39 +0100

    spec to bet

--- lib/bets/bets.ex
+++ lib/bets/bets.ex
@@ -1,6 +1,7 @@
 defmodule Bets.Bets do
   alias Bets.Bet
 
+  @spec create_bet(map) :: {:ok, Bets.Bet} || {:errors, list()}
   def create_bet(bet = %{}) do
     validated_bet = Bet.validate_bet(struct(Bet, bet))
 
